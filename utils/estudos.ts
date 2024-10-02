ec0a1d58e4a35e3d48ce54dbb3fe570a13cb8d43
Daniel Santos <daniel.santos3210@outlook.com>
2024-10-02 13:16:21 -0300

    first commit

---
+++ utils/estudos.ts
@@ -0,0 +1,60 @@
+const estudos = [
+    {
+        titulo: 'Como criar array typescript',
+        desc: 'Ola', 
+        img: './images/arrayTs.png', 
+        link: '',
+        categoria: ['typescript'], 
+        tags: 'Como criar array typescript, como faço array no typescript, como criar lista no typescript, como fazer um json no typescript'
+    },
+    { 
+        titulo: 'Como usar o useState no nextjs', 
+        desc: 'Ola', 
+        img: './images/useState.png', 
+        link: '',
+        categoria: ['nextjs', 'react'], 
+        tags: 'Como usar o useState no nextjs, useState react, useState next, useState nextjs, useState reactjs, como fazer um useState' 
+    },
+    { 
+        titulo: 'Como usar o input value', 
+        desc: 'Ola', 
+        img: './images/inputChangeValue.png', 
+        link: '',
+        categoria: ['typescript'], 
+        tags: 'Como usar o input value, como fazer barra de pesquisa no next, barra de pesquisa, pesquisar no nextjs' 
+    },
+    {
+        titulo: 'Como fazer um sistema de pesquisa onde se o texto digitado inclui',
+        desc: 'Ola',
+        img: './images/searchIncludes.png',
+        link: '',
+        categoria: ['typescript'],
+        tags: 'Como fazer um sistema de pesquisa onde se o texto digitado possui, pesquisa inclui texto digitado, search Includes'
+    },
+    {
+        titulo: 'Como abrir um projeto Next.js diretamente sem usar o terminal',
+        desc: 'Ola',
+        img: './images/abrirNextjsNoTerminal.png',
+        link: '',
+        categoria: ['nextjs'],
+        tags: 'como abrir um projeto Next.js diretamente sem usar o terminal, como abrir um arquivo next direto sem precisar do terminal?'
+    },
+    {
+        titulo: 'Como tirar print de imagens usando python selenium',
+        desc: 'Ola',
+        img: '',
+        link: 'https://pastecode.io/s/83wjw2yz',
+        categoria: ['python', 'selenium'],
+        tags: 'Como tirar print de imagens usando python selenium, automação python print, salvar imagens, automação selenium print'
+    },
+    {
+        titulo: 'Subir projeto para o github usando o terminal do git',
+        desc: 'Ola',
+        img: './images/git.png',
+        link: '',
+        categoria: ['git', 'github'],
+        tags: 'Subir projeto para o github usando o terminal do git'
+    },
+]
+
+export default estudos;
